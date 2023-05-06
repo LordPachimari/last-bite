4c44ec752ec20454db23b55e9061a9b055d61d23
aaronshenhao <aaronshenhao2001@gmail.com>
2023-05-06 21:02:18 +1000

    Marketplace draft

--- client/src/components/FoodItem.jsx
+++ client/src/components/FoodItem.jsx
@@ -2,18 +2,15 @@ import { Box, Image, Text} from '@mantine/core';
 import "./food-item.css"
 export default function FoodItem(props) {
   return (
-    <>
-      <h1>An image...3</h1>
-      <Box maw={16 * 13}>
-        <Image maw={16 * 13} height={12 * 13} radius="lg" src={props.img_path} />
-        {/*<Text>{props.name}</Text>*/}
-        <div class="uppper-text">
-          <Text truncate class="test">Pizza</Text>
-        </div>
-        <div class="lower-text">
-          <Text truncate>Pizza Hut</Text>
-        </div>
-      </Box>
-    </>
+    <Box maw={16 * 13}>
+      <Image maw={16 * 13} height={12 * 13} radius="lg" src={props.img_path} />
+      {/*<Text>{props.name}</Text>*/}
+      <div className="uppper-text">
+        <Text truncate class="test">Pizza</Text>
+      </div>
+      <div className="lower-text">
+        <Text truncate>Pizza Hut</Text>
+      </div>
+    </Box>
   )
 }
