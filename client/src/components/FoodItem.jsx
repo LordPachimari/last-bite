9421446874ebf70ae2cc749192c8dce8d3b0e4d9
aaronshenhao <aaronshenhao2001@gmail.com>
2023-05-06 19:32:58 +1000

    Styled FoodItem

--- client/src/components/FoodItem.jsx
+++ client/src/components/FoodItem.jsx
@@ -1,10 +1,11 @@
 import { Image } from '@mantine/core';
-
+import "./food-item.css"
 export default function FoodItem(props) {
   return (
     <>
-      <h1>An image...</h1>
-      <Image src={props.img_path} />
+      <h1>An image...3</h1>
+      <Image width={16*12} height={9*12} radius="lg" src={props.img_path} />
+      <div>Food</div>
     </>
   )
 }
