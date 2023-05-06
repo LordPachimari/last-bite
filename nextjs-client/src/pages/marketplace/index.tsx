0e9cbf657d4bd2ff9c1dc860f0db1350d5a25532
Ivan <opachimari@gmail.com>
2023-05-06 16:36:59 +0300

    adding nextjs

--- nextjs-client/src/pages/marketplace/index.tsx
+++ nextjs-client/src/pages/marketplace/index.tsx
@@ -1,15 +1,29 @@
+import { supabase } from "@/supabaseClient";
+import { LeftOverItem } from "@/utils/types";
 import {
   Box,
   Button,
+  Card,
   Center,
   Flex,
+  Group,
   Input,
   Space,
   Text,
   Title,
 } from "@mantine/core";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Marketplace() {
+  const [leftOvers, setLeftOvers] = useState<LeftOverItem[] | null>(null);
+  useEffect(() => {
+    const fetch = async () => {
+      const { data, error } = await supabase.from("last-bite").select("*");
+      setLeftOvers(data as LeftOverItem[] | null);
+    };
+    fetch();
+  }, []);
   return (
     <Box w="100%" h="100%">
       <Flex sx={{ alignItems: "center" }} w="100%" h={80} bg="orange.5">
@@ -40,6 +54,39 @@ export default function Marketplace() {
             Listing
           </Title>
         </Flex>
+        return (
+        <Center w="100%" h="100%" sx={{ gap: 10, flexWrap: "wrap" }}>
+          {leftOvers &&
+            leftOvers.map((item) => (
+              <Card
+                shadow="sm"
+                key={item.id}
+                padding="lg"
+                radius="md"
+                withBorder
+              >
+                <Card.Section>
+                  {item && (
+                    <Image
+                      src={JSON.parse(item.imageUrl) || ""}
+                      width={400}
+                      height={400}
+                      alt="image"
+                    />
+                  )}
+                </Card.Section>
+
+                <Group position="apart" mt="md" mb="xs">
+                  <Text weight={500}>{item?.name}</Text>
+                </Group>
+
+                <Text size="sm" color="dimmed">
+                  {item?.description}
+                </Text>
+              </Card>
+            ))}
+        </Center>
+        );
       </Box>
     </Box>
   );
