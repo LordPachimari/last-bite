cda8ab3f46d4930e16658da5eb866f95cd63057b
LordPachimari <opachimari@gmail.com>
2023-05-06 13:56:27 +1000

    adding mantine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import { RestaurantDashboard } from "./dashboard/restaurant";
+import { CompanyDashboard } from "./dashboard/company";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuItem from "./components/menuItem";
 export default function App() {
+  console.log("sefjsoefj");
   return (
-    <>
-      <h1>Welcome to LASTBITE</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<RestaurantDashboard />} />
+
+        <Route path="/companyDashboard" element={<CompanyDashboard />} />
+
+        <Route path="/dashboard/pho" element={<MenuItem />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
