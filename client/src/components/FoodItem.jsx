d8eae9a85c6e7f85df1a37826ef7b04e1a2dcb38
aaronshenhao <aaronshenhao2001@gmail.com>
2023-05-06 20:23:48 +1000

    Added text and basic styling to FoodItem, also installed Carousel.

--- client/src/components/FoodItem.jsx
+++ client/src/components/FoodItem.jsx
@@ -1,11 +1,19 @@
-import { Image } from '@mantine/core';
+import { Box, Image, Text} from '@mantine/core';
 import "./food-item.css"
 export default function FoodItem(props) {
   return (
     <>
       <h1>An image...3</h1>
-      <Image width={16*12} height={9*12} radius="lg" src={props.img_path} />
-      <div>Food</div>
+      <Box maw={16 * 13}>
+        <Image maw={16 * 13} height={12 * 13} radius="lg" src={props.img_path} />
+        {/*<Text>{props.name}</Text>*/}
+        <div class="uppper-text">
+          <Text truncate class="test">Pizza</Text>
+        </div>
+        <div class="lower-text">
+          <Text truncate>Pizza Hut</Text>
+        </div>
+      </Box>
     </>
   )
 }
--- client/src/marketplace/Marketplace.jsx
+++ client/src/marketplace/Marketplace.jsx
@@ -2,8 +2,9 @@ import FoodItem from "../components/FoodItem";
 
 export default function Marketplace() {
   
-  const exampleFood = {name: "Example",
+  const exampleFood = {food_name: "Fruits",
+                       restaurant_name: "The ocean",
                        img_path: "../assets/test.jpg"}
   
-  return <FoodItem name={exampleFood.name} img_path={exampleFood.img_path}/>
+  return <FoodItem name={exampleFood.food_name} img_path={exampleFood.img_path}/>
 }
